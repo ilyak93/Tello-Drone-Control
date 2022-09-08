5793bc6357dbee2ee7fba5e76f54f29badb52ac7
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-09-08 20:25:46 +0300

    fixed

--- DJITelloPy/tello_GT_pred_postprocess.py
+++ DJITelloPy/tello_GT_pred_postprocess.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -10,8 +12,8 @@ def rescale(xyz_GT, xyz_pred):
 # calculates the translations from the poses and the pads and also rescales the predictions according to the
 # ground-truth
 
-horizontal_dist_between_adjc_pads = 100
-vertical_dist_between_diag_adjc_pads = 50
+horizontal_dist_between_adjc_pads = 50
+vertical_dist_btw_diag_adjc_pads = 50
 
 cur_point = (0, 0)
 cur_pred = (0, 0)
@@ -30,25 +32,28 @@ with open('data/pose_GT.txt', 'r') as gt_file, open('data/pose_pred.txt', 'r') a
     prev_x_GT, prev_y_GT, prev_z_GT, prev_pad = float(prev_GT[0]), float(prev_GT[1]), float(prev_GT[2]), int(prev_GT[6])
     cur_point = (prev_x_GT, prev_y_GT)
     cur_pred = (prev_x_GT, prev_y_GT)
-    for i in range(1, len(GT_lines)-1):
+    for i in range(1, len(GT_lines) - 1):
         # calculate translation: y positive is from the left of the pad, more intuitive to align to VO where it is to
         # the right
         cur_GT = GT_lines[i].split()
         cur_x_GT, cur_y_GT, cur_z_GT, cur_pad = float(cur_GT[0]), float(cur_GT[1]), float(cur_GT[2]), int(cur_GT[6])
         assert abs(cur_pad - prev_pad) <= 3
-        #x_trans, y_trans, z_trans = 0, 0, 0
+        # x_trans, y_trans, z_trans = 0, 0, 0
         if cur_pad == prev_pad:
             x_trans, y_trans, z_trans = [cur_x_GT - prev_x_GT,
                                          -(cur_y_GT - prev_y_GT),
                                          cur_z_GT - prev_z_GT]
         else:
-            pos_dist_sign = int(abs(cur_pad - prev_pad) == 1)
-            neg_dist_sign = -int(abs(cur_pad - prev_pad) == 2)
-            sign = pos_dist_sign + neg_dist_sign
-            twice_vertical = int(abs(cur_pad - prev_pad) == 3)
-
-            cur_x_GT_relative_to_prev = cur_x_GT + vertical_dist_between_diag_adjc_pads + twice_vertical * vertical_dist_between_diag_adjc_pads
-            cur_y_GT_relative_to_prev = cur_y_GT + sign * horizontal_dist_between_adjc_pads
+            x_once = math.copysign(1, cur_pad - prev_pad)
+            x_twice = int(abs(cur_pad - prev_pad) == 3)
+            x_twice = math.copysign(x_twice, cur_pad - prev_pad)
+            dir_right = (cur_pad - prev_pad == -1 or cur_pad - prev_pad == 2)
+            horz_dist_sign = -1 if dir_right else 1
+            
+            cur_x_GT_relative_to_prev = cur_x_GT + \
+                                        x_once * vertical_dist_btw_diag_adjc_pads + \
+                                        x_twice * vertical_dist_btw_diag_adjc_pads
+            cur_y_GT_relative_to_prev = int(dir_right) * horz_dist_sign * horizontal_dist_between_adjc_pads + cur_y_GT
             x_trans, y_trans, z_trans = [cur_x_GT_relative_to_prev - prev_x_GT,
                                          -(cur_y_GT_relative_to_prev - prev_y_GT),
                                          cur_z_GT - prev_z_GT]
@@ -62,17 +67,16 @@ with open('data/pose_GT.txt', 'r') as gt_file, open('data/pose_pred.txt', 'r') a
                                 % (x_trans, y_trans, z_trans,
                                    cur_GT[3], cur_GT[4], cur_GT[5]))
 
-
         # rescale and save
-        pred = pred_lines[i-1].split()
+        pred = pred_lines[i - 1].split()
         xyz_pred = np.array([float(pred[0]), float(pred[1]), float(pred[2])])
         scaled_x, scaled_y, scaled_z = rescale(np.array([x_trans, y_trans, z_trans]), xyz_pred)
         cur_pred = [sum(x) for x in zip(cur_pred, (scaled_x, scaled_y))]
         points_pred.append(cur_pred)
 
         corrected_pred_file.write("%f %f %f %s %s %s\n"
-                                % (scaled_x, scaled_y, scaled_z,
-                                   cur_GT[3], cur_GT[4], cur_GT[5]))
+                                  % (scaled_x, scaled_y, scaled_z,
+                                     cur_GT[3], cur_GT[4], cur_GT[5]))
 
 # TODO: write plotting and saving func
 
@@ -85,10 +89,10 @@ x = np.array([pt[0] for pt in points_GT])
 y = np.array([pt[1] for pt in points_GT])
 
 # Plotting the Graph
-plt.plot(y, x)
+plt.plot(y, x, marker='o', color='b')
 plt.title("Curve plotted using the given GT points")
-plt.xlim([-100, 100])
-plt.ylim([-50, 200])
+plt.xlim([-300, 300])
+plt.ylim([-50, 400])
 plt.xlabel("Y")
 plt.ylabel("X")
 plt.show()
@@ -100,15 +104,13 @@ x = np.array([pt[0] for pt in points_pred])
 y = np.array([pt[1] for pt in points_pred])
 
 # Plotting the Graph
-plt.plot(y, x)
+plt.plot(y, x, marker='o', color='b')
 plt.title("Curve plotted using the given predicted points")
-plt.xlim([-100, 100])
-plt.ylim([-50, 200])
+plt.xlim([-300, 300])
+plt.ylim([-50, 400])
 plt.xlabel("Y")
 plt.ylabel("X")
 plt.show()
-
-
 
 # for each point colored in different color, create pic
 
