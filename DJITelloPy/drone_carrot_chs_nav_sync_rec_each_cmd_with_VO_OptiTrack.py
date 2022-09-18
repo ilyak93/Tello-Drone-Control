03c8da186032fc92d82915dd543519777fcbf60a
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-09-18 19:17:47 +0300

    added Aruco markers support + writing in previous work format
    - added Aruco markers support
    - writing in previous work format

--- DJITelloPy/drone_carrot_chs_nav_sync_rec_each_cmd_with_VO_OptiTrack.py
+++ DJITelloPy/drone_carrot_chs_nav_sync_rec_each_cmd_with_VO_OptiTrack.py
@@ -1,15 +1,34 @@
 import math
 import threading
-
 import torch
 from PIL import Image
 from djitellopy import Tello
 import time
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+import csv
+import cv2
 
 from TartanVO.Datasets.utils import Compose, CropCenter, DownscaleFlow, ToTensor, make_intrinsics_layer
 from TartanVO.TartanVO import TartanVO
-from tello_with_optitrack.position import connectOptitrack, telloState
+from tello_with_optitrack.aruco_detect import ArucoDetector
+from tello_with_optitrack.position import connectOptitrack, telloState, calc_initial_SE_motive2telloNED_inv, \
+    SE_motive2telloNED, patchState
+
+SEED = 54
+BASE_RENDER_DIR = '/home/vista/ilya_tello_test/OL_trajs_images/'
+dt_cmd = 3.
+cam_calib_fname = 'tello_960_720_calib_djitellopy.p'
+
+np.random.seed(SEED)
+render_dir = os.path.join(BASE_RENDER_DIR, str(SEED))
+
+if not os.path.exists(render_dir):
+    os.makedirs(render_dir)
+
+labels_filename = os.path.join(render_dir, 'pose_file.csv')  # For pose in VO frame
+patch_pose_VO_filename = os.path.join(render_dir, 'patch_pose_VO.csv')
 
 
 tello_intrinsics = [
@@ -59,12 +78,11 @@ unsqueeze_transform = Unsqueeze()
 body_id_drone1 = 328  # Drone's ID in Motive
 body_id_patch = 308  # Patch's ID in Motive
 
+# connect to Opti-Track
 streamingClient = connectOptitrack(body_id_drone1, body_id_patch)
 
-#opti_state = telloState(streamingClient)
-#pitch, roll, yaw = opti_state[1]
-#x, z, y = opti_state[2][0:3, 3]
-
+# Initialize the Aruco detector:
+ad = ArucoDetector(calib_filename=cam_calib_fname, aruco_type=cv2.aruco.DICT_4X4_50)
 
 tello = Tello()
 tello.connect()
@@ -81,8 +99,31 @@ tello.streamon()
 time.sleep(1)
 # take off
 tello.takeoff()
-tello.go_xyz_speed_mid(x=0, y=0, z=100, speed=20, mid=1)
+tello.go_xyz_speed_mid(x=0, y=0, z=150, speed=100, mid=1)
 time.sleep(5)
+
+tello.disable_mission_pads()
+time.sleep(0.1)
+
+reader = tello.get_frame_read()
+
+patch_detected = ad.are_4_markers_detected(reader.frame)
+print("Patch detected: " + str(patch_detected))
+
+curr_state = telloState(streamingClient)
+patch_state = patchState(streamingClient)
+
+SE_motive = curr_state[-1]  # in Y UP system
+patch_SE_motive = patch_state[-1]  # in Y UP system
+T_w_b0_inv = calc_initial_SE_motive2telloNED_inv(SE_motive)
+SE_tello_NED = np.array(SE_motive2telloNED(SE_motive, T_w_b0_inv))
+# labels_writer.writerow(list(SE_tello_NED[0]) + list(SE_tello_NED[1]) + list(SE_tello_NED[2]))
+SE_patch_NED = SE_motive2telloNED(patch_SE_motive, T_w_b0_inv)
+# patch_pose_VO_writer.writerow(list(SE_patch_NED[0]) + list(SE_patch_NED[1]) + list(SE_patch_NED[2]))
+#patch_pose_VO_file.close()
+
+data.append([reader.frame, SE_tello_NED, SE_patch_NED])
+
 data = list()
 
 write_idx = 0
@@ -93,24 +134,31 @@ response = True
 
 def writer_thread():
     global data, write_idx, planned
-    with open('data/pose_GT.txt', 'w+') as gt_file,\
-            open('data/pose_pred.txt', 'w+') as pred_file, \
-                open('data/pose_planned.txt', 'w+') as planned_file:
+    with open(labels_filename, 'w') as labels_file, \
+            open(patch_pose_VO_filename, 'w') as patch_pose_VO_file :
         while len(data) > write_idx:
             img = data[write_idx][0]
-            x, y, z, pitch, roll, yaw = data[write_idx][1]
+            SE_tello_NED = data[write_idx][1]
+            SE_patch_NED = data[write_idx][2]
             im = Image.fromarray(img)
             im.save('./data/' + str(write_idx) + '.png')
-            gt_file.write("%f %f %f %f %f %d\n" % (x, y, z, pitch, roll, yaw))
-            if write_idx >= 1:
-                predicted = data[write_idx][2]
-                pred_file.write("%f %f %f %f %f %f\n"
-                                % (predicted[0, 0], predicted[0, 1], predicted[0, 2],
-                                   predicted[0, 3], predicted[0, 4], predicted[0, 5]))
-            planned_file.write("%f %f %f\n" % (planned[write_idx][0],
-                                               planned[write_idx][1],
-                                               planned[write_idx][2]))
-            write_idx = write_idx + 1
+
+            labels_writer = csv.writer(labels_file)
+            patch_pose_VO_writer = csv.writer(patch_pose_VO_file)
+
+            labels_writer.writerow(list(SE_tello_NED[0]) + list(SE_tello_NED[1]) + list(SE_tello_NED[2]))
+            patch_pose_VO_writer.writerow(list(SE_patch_NED[0]) + list(SE_patch_NED[1]) + list(SE_patch_NED[2]))
+
+            #gt_file.write("%f %f %f %f %f %d\n" % (x, y, z, pitch, roll, yaw))
+            #if write_idx >= 1:
+            #    predicted = data[write_idx][2]
+            #    pred_file.write("%f %f %f %f %f %f\n"
+            #                    % (predicted[0, 0], predicted[0, 1], predicted[0, 2],
+            #                       predicted[0, 3], predicted[0, 4], predicted[0, 5]))
+            #planned_file.write("%f %f %f\n" % (planned[write_idx][0],
+            #                                   planned[write_idx][1],
+            #                                   planned[write_idx][2]))
+            #write_idx = write_idx + 1
 
 
 # last is False as last recording which is the first in this case have not done yet
@@ -121,17 +169,23 @@ ready = threading.Event()
 
 
 # TODO: make data a readable dict
-def recorder_thread(tello, reader):
+def recorder_thread(reader):
     global response, data, ready, focalx, focaly, centerx, centery, transform
     while True:
         ready.set()
         response.wait()
         if data[-1][1][6] == -1:
             break
-        #state = tello.get_current_state()
+        # state = tello.get_current_state()
         opti_state = telloState(streamingClient)
-        pitch, roll, yaw = opti_state[1]
-        x, z, y = opti_state[2][0:3, 3]
+        SE_motive = opti_state[-1]
+        SE_tello_NED = SE_motive2telloNED(SE_motive, T_w_b0_inv)
+        #euler = R.from_matrix(SE_tello_NED[0:3, 0:3]).as_euler('zyx', degrees=False)
+        #euler = euler / np.pi * 180.
+        #(pitch, roll, yaw) = np.flip(euler)
+
+        #x, z, y = opti_state[2][0:3, 3]
+
         cur_frame = reader.frame
         sample = {'img1': data[-1][0], 'img2': cur_frame}
         h, w, _ = cur_frame.shape
@@ -140,17 +194,17 @@ def recorder_thread(tello, reader):
         sample = transform(sample)
         sample = unsqueeze_transform(sample)
         VO_motions, VO_flow = testvo.test_batch(sample)
-        #data.append([reader.frame, (state['x'], state['y'], state['z'],
+        # data.append([reader.frame, (state['x'], state['y'], state['z'],
         #                            state["pitch"], state["roll"],
         #                            state["yaw"], state['mid']), VO_motions, [x_move, y_move, 0]])
-        data.append([reader.frame, (x, y, z, pitch, roll, yaw), VO_motions, [x_move, y_move, 0]])
+        data.append([reader.frame, SE_tello_NED, SE_patch_NED, VO_motions,
+                     [x_move, y_move, 0]])
         ready.set()
         response.clear()
 
 
-
 # start recorder and writer threads
-reader = tello.get_frame_read()
+
 recorder = threading.Thread(target=recorder_thread, args=([tello, reader]))
 recorder.start()
 
@@ -159,11 +213,6 @@ R = 25
 delta_lookahead = 50
 # calculate carrot chasing moves and send to execution
 # get first frame and its xyz label
-
-state = tello.get_current_state()
-data.append([reader.frame, (state['x'], state['y'], state['z'],
-                            state["pitch"], state["roll"],
-                            state["yaw"], state['mid'])])
 
 while True:
     # this calculatins takes 0.0 seconds
