3056fa36aa4434c809dd7de6d3a5926a332d38c2
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-09-21 20:57:52 +0300

    working version  of pose writer for optitrack

--- DJITelloPy/pose_writer check_opti.py
+++ DJITelloPy/pose_writer check_opti.py
@@ -5,6 +5,12 @@ from djitellopy import Tello
 import time
 import matplotlib.pyplot as plt
 
+from tello_with_optitrack.position import telloState, connectOptitrack, SE_motive2telloNED, \
+    calc_initial_SE_motive2telloNED_inv, SE_motive2tello_absNED, SE_motive2tello_absNED_v2, \
+    calc_initial_SE_motive2telloNED_inv_v2, SE_motive2telloNED_v2, SE_motive2telloNED_v2
+
+import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 # function for gettig the average location during 1 sec of measurements
 def get_xyz(tello):
@@ -27,39 +33,82 @@ def get_xyz(tello):
 
 
 # connect, enable missions pads detection and show battery
-print("Start")
-time.sleep(3)
-tello = Tello()
-tello.connect()
-tello.enable_mission_pads()
-time.sleep(0.1)
-tello.set_mission_pad_detection_direction(2)
-time.sleep(0.1)
+#tello = Tello()
+#tello.connect()
+#time.sleep(3)
+
+#state = tello.get_current_state()
+#print("battery is " + str(state["bat"]))
+
 
 write_idx = 0
 tello_on = True
 
+body_id_drone1 = 328  # Drone's ID in Motive
+body_id_patch = 308  # Patch's ID in Motive
 
-def writer_thread(tello):
-    global tello_on, write_idx
-    with open('./data/pose_check.txt', 'w+') as f:
+streamingClient = connectOptitrack(body_id_drone1, body_id_patch)
+
+
+#tello.takeoff()
+
+# v2 is like David explained
+time.sleep(5)
+opti_state = telloState(streamingClient)
+SE_motive = opti_state[-1]
+T_w_b0_inv = calc_initial_SE_motive2telloNED_inv(SE_motive)
+#T_w_b0_inv_v2 = calc_initial_SE_motive2telloNED_inv_v2(SE_motive)
+
+T_w_b0_abs_inv = np.array([
+ [9.89469994e-01, -2.88605423e-03,  1.44709368e-01, -3.18212964e+00],
+ [-1.44738065e-01, -1.86757113e-02,  9.89293743e-01, -2.63361071e-01],
+ [-1.52605008e-04, -9.99821428e-01, -1.88967779e-02,  1.01621539e-01],
+ [0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  1.00000000e+00]],
+    dtype=np.float64)
+
+'''T_w_b0_abs_inv_v2 = np.array([[ 0.99700721,  0.07682759,  0.00861046,  2.11780143],
+ [-0.00982162,  0.0154008,   0.99983316,  0.08217029],
+ [0.07668216, -0.99692544,  0.01610928,  1.01257479],
+ [0.,          0.,          0.,          1.]],
+    dtype=np.float64)'''
+
+
+
+def writer_thread():
+    global tello_on, write_idx, T_w_b0_inv
+    with open('./data/pose_check_opti_check1.txt', 'w+') as f:
         while True:
-            state = tello.get_current_state()
-            #x, y, z = get_xyz(tello)
-            x, y, z = state["x"], state["y"], state["z"]
-            f.write("%f %f %f pad=%d\n" % (x, y, z, state["mid"]))
+            opti_state = telloState(streamingClient)
+            SE_motive = opti_state[-1]
+            SE_tello_NED = SE_motive2telloNED(SE_motive, T_w_b0_inv)
+            SE_tello_abs_NED = SE_motive2telloNED(SE_motive, T_w_b0_abs_inv)
+            euler = R.from_matrix(SE_tello_NED[0:3, 0:3]).as_euler('zyx', degrees=False)
+            euler = euler / np.pi * 180.
+            (pitch, roll, yaw) = np.flip(euler)
+            my_euler = R.from_matrix(SE_tello_abs_NED[0:3, 0:3]).as_euler('zyx', degrees=False)
+            my_euler = my_euler / np.pi * 180.
+            (my_pitch, my_roll, my_yaw) = np.flip(my_euler)
+            #my_euler = opti_state[1]
+            #my_euler = my_euler / np.pi * 180.
+            #(my_pitch, my_roll, my_yaw) = np.flip(my_euler)
+            f.write("%f %f %f %f %f %f\n" % (pitch, roll, yaw, my_pitch, my_roll, my_yaw))
             write_idx = write_idx + 1
-            time.sleep(0.75)
+            time.sleep(0.030)
             if tello_on is False:
                 break
 
 
-writer = threading.Thread(target=writer_thread, args=([tello]))
+writer = threading.Thread(target=writer_thread, args=())
+
+print("start")
 
 writer.start()
 
+#tello.flip_forward()
+#tello.rotate_clockwise(360)
 time.sleep(15)
 
 tello_on = False
 
 writer.join()
+
