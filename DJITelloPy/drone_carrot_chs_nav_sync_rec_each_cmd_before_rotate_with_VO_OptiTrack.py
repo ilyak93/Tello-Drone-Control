d2977ee254de3d69d3d5db5ba92aafb67167f5eb
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-09-29 00:27:25 +0300

    old working version

---
+++ DJITelloPy/drone_carrot_chs_nav_sync_rec_each_cmd_before_rotate_with_VO_OptiTrack.py
@@ -0,0 +1,293 @@
+import math
+import threading
+import torch
+from PIL import Image
+from djitellopy import Tello
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import csv
+import cv2
+
+from TartanVO.Datasets.utils import Compose, CropCenter, DownscaleFlow, ToTensor, make_intrinsics_layer
+from TartanVO.TartanVO import TartanVO
+from tello_with_optitrack.aruco_detect import ArucoDetector
+from tello_with_optitrack.position import connectOptitrack, telloState, calc_initial_SE_motive2telloNED_inv, \
+    SE_motive2telloNED, patchState
+
+SEED = 54
+BASE_RENDER_DIR = '/home/vista/ilya_tello_test/OL_trajs_images/'
+dt_cmd = 3.
+cam_calib_fname = 'tello_960_720_calib_djitellopy.p'
+
+np.random.seed(SEED)
+render_dir = os.path.join(BASE_RENDER_DIR, str(SEED))
+
+if not os.path.exists(render_dir):
+    os.makedirs(render_dir)
+
+labels_filename = os.path.join(render_dir, 'pose_file.csv')  # For pose in VO frame
+patch_pose_VO_filename = os.path.join(render_dir, 'patch_pose_VO.csv')
+
+# TODO: add rotations to carrot chasing and correct recording accordingly
+
+# TODO: make carrot chasing relative to chosen y and z of the target
+
+# TODO: add carrot chasing with z-axis
+
+tello_intrinsics = [
+    [785.75708966, 0., 494.5589324],
+    [0., 781.95811828, 319.88369613],
+    [0., 0., 1.]
+]
+
+testvo = TartanVO("tartanvo_1914.pkl")
+focalx, focaly, centerx, centery = 785.75708966, 781.95811828, 494.5589324, 319.88369613
+
+
+class Unsqueeze(object):
+    """
+    Scale the flow and mask to a fixed size
+    """
+
+    def __init__(self, axis=0):
+        '''
+        size: output frame size, this should be NO LARGER than the input frame size!
+        '''
+        self.axis = axis
+
+    def __call__(self, sample):
+        for key in sample.keys():
+            if key != 'motion':
+                sample[key] = sample[key].unsqueeze(self.axis).cuda()
+            else:
+                sample[key] = sample[key].unsqueeze(self.axis)
+        return sample
+
+
+image_width, image_height = 640, 448
+transform = Compose([CropCenter((image_height, image_width)), DownscaleFlow(), ToTensor()])
+unsqueeze_transform = Unsqueeze()
+
+# first run the script to get initial_rotation_view
+# which should be aligned to the target (directly in front of it)
+initial_rotation_view = np.load("initial_rotation_view.npy")
+
+# how to run the VO on VO:
+# res = {'img1': img1, 'img2': img2 }
+# h, w, _ = img1.shape
+# intrinsicLayer = make_intrinsics_layer(w, h, self.focalx, self.focaly, self.centerx, self.centery)
+# res['intrinsic'] = intrinsicLayer
+# res = transform(res)
+# res['motion'] = groundTruth
+
+# connect, enable missions pads detection and show battery
+body_id_drone1 = 328  # Drone's ID in Motive
+body_id_patch = 308  # Patch's ID in Motive
+
+# connect to Opti-Track
+streamingClient = connectOptitrack(body_id_drone1, body_id_patch)
+
+if not streamingClient:
+    print("Optitrack connection error")
+    exit(-1)
+
+# Initialize the Aruco detector:
+ad = ArucoDetector(calib_filename=cam_calib_fname, aruco_type=cv2.aruco.DICT_4X4_50)
+
+tello = Tello()
+tello.connect()
+tello.enable_mission_pads()
+time.sleep(0.1)
+tello.set_mission_pad_detection_direction(2)
+time.sleep(0.1)
+
+state = tello.get_current_state()
+print("battery is " + str(state["bat"]))
+
+# enable video
+tello.streamon()
+time.sleep(1)
+# take off
+tello.takeoff()
+time.sleep(3)
+tello.go_xyz_speed_mid(x=0, y=0, z=150, speed=20, mid=1)
+time.sleep(5)
+
+tello.disable_mission_pads()
+time.sleep(0.1)
+
+data = list()
+
+reader = tello.get_frame_read()
+
+cur_frame = reader.frame
+curr_state = telloState(streamingClient)
+patch_state = patchState(streamingClient)
+
+SE_motive = curr_state[-1]  # in Y UP system
+patch_SE_motive = patch_state[-1]  # in Y UP system
+T_w_b0_inv = calc_initial_SE_motive2telloNED_inv(SE_motive)
+SE_tello_NED = np.array(SE_motive2telloNED(SE_motive, T_w_b0_inv))
+# labels_writer.writerow(list(SE_tello_NED[0]) + list(SE_tello_NED[1]) + list(SE_tello_NED[2]))
+SE_patch_NED = SE_motive2telloNED(patch_SE_motive, T_w_b0_inv)
+# patch_pose_VO_writer.writerow(list(SE_patch_NED[0]) + list(SE_patch_NED[1]) + list(SE_patch_NED[2]))
+# patch_pose_VO_file.close()
+
+data.append([cur_frame, SE_tello_NED, SE_patch_NED])
+
+m_to_cm = 100
+
+target_translation_from_initial = (250, 0, 0)  # in meters
+
+target_pos = data[-1][1][0:3, 3] + target_translation_from_initial
+
+initial_pos = data[-1][1][0:3, 3]
+
+print("target_pos pose " + str(target_pos))
+
+patch_detected = ad.are_4_markers_detected(data[-1][0])
+print("Patch detected: " + str(patch_detected))
+
+write_idx = 0
+planned = list()
+# reponse is True as the last command of taking off with alignment using go mid finished
+response = True
+
+
+def writer_thread():
+    global data, write_idx, planned
+    with open(labels_filename, 'w') as labels_file, \
+            open(patch_pose_VO_filename, 'w') as patch_pose_VO_file:
+        labels_writer = csv.writer(labels_file)
+        while len(data) > write_idx:
+            img = data[write_idx][0]
+            SE_tello_NED = data[write_idx][1]
+
+            im = Image.fromarray(img)
+            im.save(BASE_RENDER_DIR + str(write_idx) + '.png')
+
+            labels_writer.writerow(list(SE_tello_NED[0]) + list(SE_tello_NED[1]) + list(SE_tello_NED[2]))
+            write_idx = write_idx + 1
+        patch_pose_VO_writer = csv.writer(patch_pose_VO_file)
+        SE_patch_NED = data[0][2]
+        patch_pose_VO_writer.writerow(list(SE_patch_NED[0]) + list(SE_patch_NED[1]) + list(SE_patch_NED[2]))
+
+        # gt_file.write("%f %f %f %f %f %d\n" % (x, y, z, pitch, roll, yaw))
+        # if write_idx >= 1:
+        #    predicted = data[write_idx][2]
+        #    pred_file.write("%f %f %f %f %f %f\n"
+        #                    % (predicted[0, 0], predicted[0, 1], predicted[0, 2],
+        #                       predicted[0, 3], predicted[0, 4], predicted[0, 5]))
+        # planned_file.write("%f %f %f\n" % (planned[write_idx][0],
+        #                                   planned[write_idx][1],
+        #                                   planned[write_idx][2]))
+
+
+# last is False as last recording which is the first in this case have not done yet
+last = False
+
+response = threading.Event()
+ready = threading.Event()
+
+target_radius = 30
+
+
+# TODO: make data a readable dict
+def recorder_thread(reader):
+    global response, data, ready, focalx, focaly, centerx, centery, transform
+    while True:
+        ready.set()
+        response.wait()
+        cur_pose = data[-1][1][0:3, 3] * m_to_cm
+        print("dist from target " + str(math.sqrt(sum((cur_pose[:2] - target_pos[:2]) ** 2))))
+        if math.sqrt(sum((cur_pose[:2] - target_pos[:2]) ** 2)) <= target_radius:
+            break
+        opti_state = telloState(streamingClient)
+        SE_motive = opti_state[-1]
+        SE_tello_NED = SE_motive2telloNED(SE_motive, T_w_b0_inv)
+        # euler = R.from_matrix(SE_tello_NED[0:3, 0:3]).as_euler('zyx', degrees=False)
+        # euler = euler / np.pi * 180.
+        # (pitch, roll, yaw) = np.flip(euler)
+
+        # x, z, y = opti_state[2][0:3, 3]
+
+        cur_frame = reader.frame
+        sample = {'img1': data[-1][0], 'img2': cur_frame}
+        h, w, _ = cur_frame.shape
+        intrinsicLayer = make_intrinsics_layer(w, h, focalx, focaly, centerx, centery)
+        sample['intrinsic'] = intrinsicLayer
+        sample = transform(sample)
+        sample = unsqueeze_transform(sample)
+        VO_motions, VO_flow = testvo.test_batch(sample)
+        # data.append([reader.frame, (state['x'], state['y'], state['z'],
+        #                            state["pitch"], state["roll"],
+        #                            state["yaw"], state['mid']), VO_motions, [x_move, y_move, 0]])
+        data.append([cur_frame, SE_tello_NED, VO_motions,
+                     [x_move, y_move, 0]])
+
+        print("current pos is " + str(cur_pose))
+        ready.set()
+        response.clear()
+
+
+# start recorder and writer threads
+
+recorder = threading.Thread(target=recorder_thread, args=([reader]))
+recorder.start()
+
+distance_btw_pads = 50
+R = 25
+delta_lookahead = 50
+# calculate carrot chasing moves and send to execution
+# get first frame and its xyz label
+
+while True:
+    # this calculatins takes 0.0 seconds
+    # start = time.time()
+    # print("loc = " + str(executed[-1]))
+    (cur_x, cur_y, cur_z) = data[-1][1][0:3, 3] * m_to_cm
+    cur_poz = (cur_x, cur_y, cur_z)
+    x_move, y_move = R, 0
+    if cur_y != 0:
+        tan_alpha = delta_lookahead / abs(cur_y)
+        # (tan_alpha+1)*y**2 = R**2 --> y = math.sqrt(R**2 / (tan_alpha+1))
+        y_move_abs = math.sqrt(R ** 2 / (tan_alpha + 1))
+        y_move = float(y_move_abs) if cur_y > 0 else float(-y_move_abs)
+        x_move = math.sqrt(R ** 2 - y_move ** 2)
+        # print("xmove and ymove are: " + str(x_move) + ',' + str(y_move))
+        if abs(x_move) < 20.0 and abs(y_move) < 20.0:
+            if abs(x_move) > abs(y_move):
+                x_move = math.copysign(20.0, x_move)
+            else:
+                y_move = math.copysign(20.0, y_move)
+
+    # end = time.time()
+    # print("time is" + str(end - start))
+    planned.append((round(x_move), round(y_move), 0))
+
+    ready.wait()
+    if math.sqrt(sum((cur_poz[:2] - target_pos[:2]) ** 2)) <= target_radius:
+        response.set()
+        break
+    tello.go_xyz_speed(x=round(x_move), y=round(y_move), z=0, speed=20)
+    time.sleep(3)
+    ready.clear()
+    response.set()
+    ready.wait()
+    patch_detected = ad.are_4_markers_detected(data[-1][0])
+    print("Patch detected: " + str(patch_detected))
+
+tello.land()
+tello.end()
+recorder.join()
+
+writer = threading.Thread(target=writer_thread, args=())
+writer.start()
+writer.join()
+
+# signals on response and signals on executed :
+# carrot chasing should sleep_wait until gets a signal from recorder
+# that it recorded the last True executed command
+# recorder should sleep_wait while command yet sent to tello drone
