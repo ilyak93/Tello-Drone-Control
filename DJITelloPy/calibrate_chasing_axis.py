a89399456c290a422907a6db1a69edc6c9635fef
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-10-30 00:37:18 +0300

    pre-computing and saving target coordinates
    - make available choosing height even higher then actual wanted target height for changing the effective height the drone fly to because of too small values with carrot chasing  (enlarging the magnitude of the z-value for bolding it for carrot chasing computations)

--- DJITelloPy/calibrate_chasing_axis.py
+++ DJITelloPy/calibrate_chasing_axis.py
@@ -1,13 +1,18 @@
 # connect, enable missions pads detection and show battery
 import numpy as np
 from scipy.spatial.transform import Rotation as Rot
+from skspatial.objects import Line, Sphere
 
-from tello_with_optitrack.position import connectOptitrack, telloState, calc_initial_SE_motive2telloNED_inv, \
+from tello_with_optitrack.position import connectOptitrack, telloState, \
     SE_motive2telloNED
 import math
+
 body_id_drone1 = 334  # Drone's ID in Motive
 body_id_patch = 308  # Patch's ID in Motive
 
+m_to_cm = 100
+
+initial_center_y = np.load("center_y_axis.npy")
 # connect to Opti-Track
 streamingClient = connectOptitrack(body_id_drone1, body_id_patch)
 
@@ -18,16 +23,35 @@ if not streamingClient:
 m_to_cm = 100
 opti_state = telloState(streamingClient)
 SE_motive = opti_state[-1]
-x, z, y = SE_motive[0:3, 3] * m_to_cm
-cur_x, cur_y = -x, -y
+x, _, y = SE_motive[0:3, 3] * m_to_cm
+initial_x, initial_y = -x, -y
+takeoff_height = 140
 
+target_translation = 700  # target
 
-initial_center_y = np.load("center_y_axis.npy")
-delta_x = 500
-delta_y = cur_y - initial_center_y
-slope = -(delta_x / delta_y) if delta_y < 0 else delta_x / delta_y
+# (x, y, z, pitch, roll, yaw) : (cm, cm, cm, deg, deg, deg)
+target_z_to_choose = 220
+target_pos = np.asarray([initial_x + target_translation, initial_center_y,
+                         target_z_to_choose])
 
+start_point3D = (initial_y, initial_x, takeoff_height)
+target_x, target_y, target_z = target_pos[0:3]
 
+source_to_target_chase_axis = Line.from_points(point_a=start_point3D,
+                                               point_b=np.array((target_y,
+                                                                 target_x,
+                                                                 target_z)))
+
+R = target_translation
+sphere = Sphere(start_point3D, R)
+point_a, point_b = sphere.intersect_line(source_to_target_chase_axis)
+
+actual_target = point_a if point_a[1] > point_b[1] else point_b
+
+#delta_x = target_translation
+delta_x = actual_target[1] - initial_x
+#delta_y = initial_y - initial_center_y
+delta_y = initial_y - actual_target[0]
 alpha = 0
 
 initial_rotation_view = np.load("initial_rotation_view.npy")
@@ -44,7 +68,10 @@ if delta_y != 0:
     alpha_deg = alpha_deg if delta_y < 0 else -alpha_deg
     alpha = alpha_deg - yaw
 
-np.save("slope_bias_alpha_start_pos.npy", np.array((slope, delta_y, alpha, cur_x, cur_y, z)))
+np.save("alpha_start_pos_target_pos.npy", np.array((alpha, initial_x,
+                                                    initial_y, takeoff_height,
+                                                    actual_target[1],
+                                                    actual_target[0],
+                                                    actual_target[2])))
 print("turn ur drone to postion of " + str(alpha) + "degrees right (negative is left correspondingly")
-
 
