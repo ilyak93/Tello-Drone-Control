262614c91177e70f1c6ac887f868a1beaa9decaa
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-10-30 02:48:56 +0200

    Update calibrate_rotation_view.py

--- DJITelloPy/calibrate_rotation_view.py
+++ DJITelloPy/calibrate_rotation_view.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from tello_with_optitrack.position import connectOptitrack, telloState, calc_initial_SE_motive2telloNED_inv
 
-body_id_drone1 = 334  # Drone's ID in Motive
+body_id_drone1 = 333  # Drone's ID in Motive
 body_id_patch = 308  # Patch's ID in Motive
 
 # connect to Opti-Track
@@ -13,11 +13,11 @@ if not streamingClient:
     print("Optitrack connection error")
     exit(-1)
 
-
 opti_state = telloState(streamingClient)
 SE_motive = opti_state[-1]
 initial_rotation_view = calc_initial_SE_motive2telloNED_inv(SE_motive)
 
 np.save("carrot_chasing_rotation_view.npy", initial_rotation_view)
+
 
 
