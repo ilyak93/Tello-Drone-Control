1865d95d50c7ecc4258bf270e7effc5082dc4870
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-12-14 03:15:28 +0200

    minor modifications

--- DJITelloPy/animation_w_OptiTrack_xz.py
+++ DJITelloPy/animation_w_OptiTrack_xz.py
@@ -3,9 +3,6 @@ import math
 import numpy as np
 import os
 
-from TartanVO.Datasets.transformation import ses2poses_quat, pos_quats2SE_matrices, motion2pose
-
-
 # Rescaling as it is done in TartanVO as the final step if sample has "motion" key, i.e label
 def rescale(xyz_GT, xyz_pred):
     scale = np.linalg.norm(xyz_GT)
@@ -25,8 +22,8 @@ points_planned = list()
 SEED = 54
 BASE_RENDER_DIR = '/home/vista/ilya_tello_test/OL_trajs_images/'
 render_dir = os.path.join(BASE_RENDER_DIR, str(SEED))
-viz_dir = os.path.join(render_dir, 'viz')
-anim_dir = os.path.join(viz_dir, 'anim')
+viz_dir = os.path.join(render_dir,'viz')
+anim_dir = os.path.join(viz_dir, 'anim_xz')
 pose_GT = os.path.join(viz_dir, 'pose_GT.txt')
 pose_pred = os.path.join(viz_dir, 'pose_pred.txt')
 pose_planned = os.path.join(viz_dir, 'pose_planned.txt')
@@ -44,18 +41,18 @@ with open(pose_GT, 'r') as gt_file, \
     pred_lines = [line.rstrip() for line in pred_file]
     prev_GT = GT_lines[0].split()
     prev_x_GT, prev_y_GT, prev_z_GT = float(prev_GT[0]), float(prev_GT[1]), float(prev_GT[2])
-    cur_point = (prev_x_GT, prev_y_GT)
-    cur_pred = (prev_x_GT, prev_y_GT)
+    cur_point = (prev_x_GT, prev_z_GT)
+    cur_pred = (prev_x_GT, prev_z_GT)
     points_GT.append(cur_point)
     planned_lines = [line.rstrip() for line in planned_file]
     cur_planned = planned_lines[0].split()
     points_planned.append((cur_point[0] + float(cur_planned[0]),
-                           cur_point[1] + -float(cur_planned[1])))
+                           cur_point[1] + float(cur_planned[2])))
 
     corrected_planned_file.write("%f %f %f %f\n"
                                  % (cur_point[0], cur_point[1],
                                     float(cur_planned[0]),
-                                    -float(cur_planned[1])))
+                                    float(cur_planned[2])))
 
     for i in range(1, len(GT_lines)):
         # calculate translation: y positive is from the left of the pad, more intuitive to align to VO where it is to
@@ -69,17 +66,17 @@ with open(pose_GT, 'r') as gt_file, \
                                      cur_y_GT - prev_y_GT,
                                      cur_z_GT - prev_z_GT]
 
-        cur_point = [sum(x) for x in zip(cur_point, (x_trans, y_trans))]
+        cur_point = [sum(x) for x in zip(cur_point, (x_trans, z_trans))]
         points_GT.append(cur_point)
 
         cur_planned = planned_lines[i].split()
         points_planned.append((cur_point[0] + float(cur_planned[0]),
-                               cur_point[1] - float(cur_planned[1])))
+                               cur_point[1] + float(cur_planned[2])))
 
         corrected_planned_file.write("%f %f %f %f\n"
                                      % (cur_point[0], cur_point[1],
                                         float(cur_planned[0]),
-                                        -float(cur_planned[1])))
+                                        float(cur_planned[2])))
 
 
         prev_x_GT, prev_y_GT, prev_z_GT = cur_x_GT, cur_y_GT, cur_z_GT
@@ -90,13 +87,9 @@ with open(pose_GT, 'r') as gt_file, \
 
         # rescale and save
         pred = pred_lines[i - 1].split()
-        #pred = [float(p) for p in pred_lines[i - 1].split()]
-        #pred_format = np.array(pred).reshape((1,-1))
-        #motions = motion2pose(pred_format)  # for 2 images, we get after this line 2 motions, one being the identity
-        #cur_pred = motions[-1]  # hence we take the latter
-        xyz_pred = np.array([float(pred[0]), float(pred[1]), float(pred[2])])
-        scaled_x, scaled_y, scaled_z = rescale(np.array([x_trans, y_trans, z_trans]), xyz_pred)
-        cur_pred = [sum(x) for x in zip(cur_pred, (scaled_x, scaled_y))]
+        xzy_pred = np.array([float(pred[0]), float(pred[2]), float(pred[1])])
+        scaled_x, scaled_z, scaled_y = rescale(np.array([x_trans, z_trans, y_trans]), xzy_pred)
+        cur_pred = [sum(x) for x in zip(cur_pred, (scaled_x, scaled_z))]
         points_pred.append(cur_pred)
 
         corrected_pred_file.write("%f %f %f %s %s %s\n"
@@ -112,32 +105,36 @@ import matplotlib.pyplot as plt
 
 # Dataset
 
-_, x_start, y_start, _, target_x, target_y, _, x_stop, \
+_, x_start, _, z_start, target_x, _, target_z, x_stop, \
     = np.load("alpha_start_pos_target_pos_x_stop.npy")
 
 chasing_x_points = [x_start, target_x]
-chasing_y_points = [y_start, target_y]
+chasing_z_points = [z_start, target_z]
 
 chasing_stop_plane = x_stop
 
 x_GT = np.array([pt[0] for pt in points_GT])
-y_GT = np.array([pt[1] for pt in points_GT])
+z_GT = np.array([pt[1] for pt in points_GT])
 
 x_pred_l = [pt[0] for pt in points_pred]
 
 x_pred = np.array(x_pred_l)
 
-y_pred_l = [pt[1] for pt in points_pred]
+z_pred_l = [pt[1] for pt in points_pred]
 
-y_pred = np.array(y_pred_l)
+z_pred = np.array(z_pred_l)
 
-x_planned= [pt[0] for pt in points_planned[:-1]]
-y_planned = [pt[1] for pt in points_planned[:-1]]
-
+x_planned = [pt[0] for pt in points_planned[:-1]]
+z_planned = [pt[1] for pt in points_planned[:-1]]
 
 # Plotting the Graph
 plt.rcParams["figure.figsize"] = [3*6.4,3*6.4]
 plt.rcParams['font.size'] = 10
+
+chasing_x_points = [x_start, target_x]
+chasing_y_points = [z_start, target_z]
+
+chasing_stop_plane = x_stop
 
 plt.plot(chasing_y_points, chasing_x_points, linestyle="--", marker='p', color='k')
 plt.axhline(y=x_stop, color='k', linestyle='--')
@@ -150,14 +147,14 @@ plt.tick_params(axis='both', which='major', labelsize=10)
 for i in range(len(x_GT)):
     # Plotting the Graph
     cur_x_GT = x_GT[:i+1]
-    cur_y_GT = y_GT[:i+1]
+    cur_y_GT = z_GT[:i+1]
     plt.plot(cur_y_GT, cur_x_GT, marker='o', color='b')
     for i, xy in enumerate(zip(cur_y_GT, cur_x_GT)):
        plt.annotate('%d' % i, xy=xy)
     if i > 0:
-        plt.plot(y_pred[i-1], x_pred[i-1], marker='x', color='r')
+        plt.plot(z_pred[i-1], x_pred[i-1], marker='x', color='r')
     if i < len(x_GT)-2:
-        plt.scatter(y_planned[i], x_planned[i], marker='v', color='g')
+        plt.scatter(z_planned[i], x_planned[i], marker='v', color='g')
     plt.title("Groudtruth locations, Visual Odometry estimations and planned navigation")
     plt.xlim([-250,150])
     plt.xticks(list(range(-250, 150, 5)))
@@ -172,4 +169,3 @@ import matplotlib.pyplot as plt
 
 
 # for each point colored in different color, create pic
-
