2cf1917c7d20b85d3b845434000a65f4e582ef3f
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2022-12-15 01:01:07 +0200

    added TODO

--- DJITelloPy/process_real_images/process_directories.py
+++ DJITelloPy/process_real_images/process_directories.py
@@ -3,11 +3,14 @@
 import os
 
 script_name = 'gen_dataset_from_Aruco_images.py'
-base_src_dir = 'C:/Users/vista/Desktop/DJI_Album/TELLO/OL_speed_60_190522_big_target/'
-base_output_dir = 'C:/Users/vista/Desktop/DJI_Album/OL_speed_60_190522_big_target_proc/'
+base_src_dir = '/home/vista/ilya_tello_test/trajs_3D/dataset/OL_trajs_images_R25_btt_center_2/'
+base_output_dir = '/home/vista/ilya_tello_test/trajs_3D/dataset/OL_trajs_images_R25_btt_center_2_proc/'
 
 src_dirs = [os.path.join(base_src_dir, o) for o in os.listdir(base_src_dir)]
 
 for src_dir in src_dirs:
     output_dir = os.path.join(base_output_dir, src_dir.split('/')[-1])
     os.system('python ' + script_name + ' ' + src_dir + ' ' + output_dir)
+
+
+#TODO check the missing frame sometimes
